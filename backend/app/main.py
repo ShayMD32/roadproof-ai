0bbc3da1b4221c2166160d67c4b956e333e16c24
shay <p2870804@my365.dmu.ac.uk>
2026-08-06 16:18:47 +0100

    Add vehicle model and POST endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+class Vehicle(BaseModel):
+    registration: str
+    make: str
+    model: str
+    year: int
 
 
 @app.get("/")
 def home():
     return {
-        "message": "Welcome to RoadProof AI!"
+        "message": "Welcome to RoadProof AI! 🚗 "
+    }
+@app.get("/health")
+def health():
+    return {
+        "status": "Server Running",
+        "version": "1.0"
+    }
+
+@app.post("/vehicle")
+def create_vehicle(vehicle: Vehicle):
+    return {
+        "message": "Vehicle received successfully!",
+        "vehicle": vehicle
     }
